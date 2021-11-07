40c22105f639e92e02489b54499803cd2e85ab55
sriramchaudhury@gmail.com <sriramchaudhury@gmail.com>
2021-11-07 18:29:04 +0530

    updated run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #English hindi default dictionary
 #lt-proc for getting morph of english words
 
-utf8_wx E_H_sen.txt | sed -e 's/Z//'  > E_H_sen-wx.txt
+#utf8_wx E_H_sen.txt | sed -e 's/Z//'  > E_H_sen-wx.txt
 cut -f 1 E_H_sen-wx.txt > eng
 cut -f 2 E_H_sen-wx.txt > hnd
 lt-proc -a en.morf.bin eng > eng_morph
